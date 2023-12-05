95904dc688bf0afc12d8be860f28840d2c8e6d55
Seongwoo Kang <maxkangdev@gmail.com>
2023-12-05 19:46:45 +0900

    setup in-memory authentication

--- springboot-blog/src/main/java/com/springboot/blog/controller/PostController.java
+++ springboot-blog/src/main/java/com/springboot/blog/controller/PostController.java
@@ -3,11 +3,11 @@ package com.springboot.blog.controller;
 import com.springboot.blog.payload.PostDto;
 import com.springboot.blog.payload.PostResponse;
 import com.springboot.blog.service.PostService;
-import com.springboot.blog.utils.AppConstants;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,6 +21,7 @@ public class PostController {
 
     private PostService postService;
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
     public ResponseEntity<PostDto> createPost(@Valid @RequestBody PostDto postDto){
         return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
@@ -41,12 +42,14 @@ public class PostController {
         return postService.getAllPosts(pageNo, pageSize, sortBy,sortDir);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{id}")
     public ResponseEntity<PostDto> updatePost(@Valid @RequestBody PostDto postDto, @PathVariable(name="id")long id){
         PostDto postResponse = postService.updatePost(postDto, id);
         return new ResponseEntity<>(postResponse, HttpStatus.OK);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deletePost(@PathVariable(name="id")long id){
         postService.deletePostById(id);
