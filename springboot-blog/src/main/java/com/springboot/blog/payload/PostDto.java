cf177d6399eeeabb44d3172edb87320ab5e5054f
Seongwoo Kang <maxkangdev@gmail.com>
2023-12-09 22:31:15 +0900

    Add swagger description for PostDto

--- springboot-blog/src/main/java/com/springboot/blog/payload/PostDto.java
+++ springboot-blog/src/main/java/com/springboot/blog/payload/PostDto.java
@@ -2,6 +2,7 @@ package com.springboot.blog.payload;
 
 
 import com.springboot.blog.entity.Post;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -10,6 +11,9 @@ import org.modelmapper.ModelMapper;
 
 import java.util.Set;
 
+@Schema(
+        description = "PostDto Model Information"
+)
 @Getter
 @Setter
 @Builder
@@ -18,19 +22,24 @@ import java.util.Set;
 public class PostDto {
     private Long id;
 
+    @Schema(description = "Blog Post Title")
     @NotEmpty
     @Size(min= 2, message = "Post title should have at least 2 characters")
     private String title;
 
+    @Schema(description = "Blog Post Description")
     @NotNull
     @NotEmpty
     @Size(min= 10, message = "Post description should have at least 10 characters")
     private String description;
 
+    @Schema(description = "Blog Post Content")
     @NotEmpty
     @NotNull
     private String content;
     private Set<CommentDto> comments;
+
+    @Schema(description = "Blog Post Category")
     private Long categoryId;
 
     private static ModelMapper mapper = new ModelMapper();
