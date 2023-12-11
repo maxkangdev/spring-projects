de8d9f0d205641755b2e7ea965e174c36079a9a0
Seongwoo Kang <maxkangdev@gmail.com>
2023-12-11 21:59:01 +0900

    add security description to category and comment controllers

--- springboot-blog/src/main/java/com/springboot/blog/controller/CategoryController.java
+++ springboot-blog/src/main/java/com/springboot/blog/controller/CategoryController.java
@@ -3,6 +3,9 @@ package com.springboot.blog.controller;
 
 import com.springboot.blog.payload.CategoryDto;
 import com.springboot.blog.service.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.apache.coyote.Response;
@@ -21,30 +24,78 @@ public class CategoryController {
 
     private CategoryService categoryService;
 
+    @Operation(
+            summary = "Post Category REST API",
+            description = "Insert new category into the database"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "Insertion Complete"
+    )
+    @SecurityRequirement(
+            name = "Bearer Authentication"
+    )
     @PostMapping
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<CategoryDto> addCategory(@RequestBody CategoryDto categoryDto){
         CategoryDto savedCategory = categoryService.addCategory(categoryDto);
         return new ResponseEntity<>(savedCategory, HttpStatus.CREATED);
     }
-
+    @Operation(
+            summary = "Get Category REST API",
+            description = "Get category with ID from the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Get Complete"
+    )
     @GetMapping("/{id}")
     public ResponseEntity<CategoryDto> getCategory(@PathVariable(value = "id") Long categoryId){
         CategoryDto category = categoryService.getCategory(categoryId);
         return new ResponseEntity<>(category, HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Get All Categories REST API",
+            description = "Get categories from the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Get Complete"
+    )
     @GetMapping("")
     public ResponseEntity<List<CategoryDto>> getAllCategories(){
         return new ResponseEntity<>(categoryService.getAllCategories(), HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Update Category REST API",
+            description = "Update category with ID from the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Update Complete"
+    )
+    @SecurityRequirement(
+            name = "Bearer Authentication"
+    )
     @PutMapping("/{id}")
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<CategoryDto> updateCategory(@RequestBody CategoryDto categoryDto, @PathVariable(value = "id") Long categoryId){
         return ResponseEntity.ok(categoryService.updateCategory(categoryDto, categoryId));
     }
 
+    @Operation(
+            summary = "Delete Category REST API",
+            description = "Delete category with ID from the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Delete Complete"
+    )
+    @SecurityRequirement(
+            name = "Bearer Authentication"
+    )
     @DeleteMapping("/{id}")
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<String> deleteCategory(@PathVariable(value = "id") Long categoryId){
--- springboot-blog/src/main/java/com/springboot/blog/controller/CommentController.java
+++ springboot-blog/src/main/java/com/springboot/blog/controller/CommentController.java
@@ -3,6 +3,9 @@ package com.springboot.blog.controller;
 
 import com.springboot.blog.payload.CommentDto;
 import com.springboot.blog.service.CommentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
@@ -22,27 +25,63 @@ public class CommentController {
 
     private CommentService commentService;
 
+    @Operation(
+            summary = "Post Comment REST API",
+            description = "Insert new comment to post with id into the database"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "Insertion Complete"
+    )
+    @SecurityRequirement(
+            name = "Bearer Authentication"
+    )
     @PreAuthorize("hasRole('USER')")
     @PostMapping("/posts/{postId}/comments")
     public ResponseEntity<CommentDto> createComment(@PathVariable(value = "postId") long postId, @Valid @RequestBody CommentDto commentDto){
         return new ResponseEntity<>(commentService.createComment(postId, commentDto), HttpStatus.CREATED);
     }
 
-    @PreAuthorize("hasRole('USER')")
+    @Operation(
+            summary = "Get Comment REST API",
+            description = "Get particular comment from the post from the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Get Complete"
+    )
     @GetMapping("/posts/{postId}/comments/{commentId}")
     public ResponseEntity<CommentDto> getComentById(@PathVariable(value = "postId") long postId, @PathVariable(value = "commentId") long commentId){
         CommentDto commentDto = commentService.getCommentById(postId, commentId);
         return new ResponseEntity<>(commentDto,HttpStatus.OK);
     }
 
-    @PreAuthorize("hasRole('USER')")
+    @Operation(
+            summary = "Get All comments REST API",
+            description = "Get all comments from the post with id from the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Get Complete"
+    )
     @GetMapping("/posts/{postId}/comments")
-    public List<CommentDto> getCommentsByPostId(@PathVariable(value = "postId")long postId){
-        return commentService.getCommentsByPostId(postId);
+    public ResponseEntity<List<CommentDto>> getCommentsByPostId(@PathVariable(value = "postId")long postId){
+        return new ResponseEntity<>(commentService.getCommentsByPostId(postId),HttpStatus.OK);
     }
 
 
-    @PreAuthorize("hasRole('USER')")
+    @Operation(
+            summary = "Update comment from certain post REST API",
+            description = "Update particular comment from the post"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Get Complete"
+    )
+    @SecurityRequirement(
+            name = "Bearer Authentication"
+    )
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/posts/{postId}/comments/{commentId}")
     public ResponseEntity<CommentDto> updateComment(@PathVariable(value = "postId")long postId,
                                                     @PathVariable(value = "commentId")long commentId,
@@ -51,6 +90,17 @@ public class CommentController {
         return new ResponseEntity<>(updatedComment,HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Delete comment from certain post REST API",
+            description = "Delete particular comment from the post"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Delete Complete"
+    )
+    @SecurityRequirement(
+            name = "Bearer Authentication"
+    )
     @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/posts/{postId}/comments/{commentId}")
     public ResponseEntity<String> deleteComment(@PathVariable(value = "postId")long postId,
