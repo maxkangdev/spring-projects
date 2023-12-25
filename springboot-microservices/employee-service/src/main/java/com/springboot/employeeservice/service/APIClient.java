263db8d4a6c3a6c8da5e796380428b54fc9aa442
Seongwoo Kang <maxkangdev@gmail.com>
2023-12-25 15:32:24 +0900

    connect employee service to eureka server

--- springboot-microservices/employee-service/src/main/java/com/springboot/employeeservice/service/APIClient.java
+++ springboot-microservices/employee-service/src/main/java/com/springboot/employeeservice/service/APIClient.java
@@ -7,7 +7,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url = "http://localhost:8080", value = "DEPARTMENT-SERVICE")
+@FeignClient(name = "DEPARTMENT" +
+        "" +
+        "ERVICE")
 public interface APIClient {
     @GetMapping("api/departments/{department-code}")
     public ResponseEntity<DepartmentDto> getDepartment(@PathVariable("department-code") String departmentCode);
